ad3c0a87e657dfd47c5b542d88f0d93e8277fde0
Anusha <anushaanand1991@gmail.com>
2022-05-12 18:46:47 +0530

    Roman numeral lookup

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const app = express();
 
-
-app.get("/", (req,res,next)=>{
-    
-    res.send("Server Running");   
+app.get("/:number", (req,res,next)=>{
+    const romanNumerals = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+      };
+    var quotient, result='';
+    var num = req.params.number;
+    var numLookup = Object.values(romanNumerals);
+    var romanLookUp = Object.keys(romanNumerals);
+    for(var i=0;i<=numLookup.length-1;i++) {
+        quotient = Math.floor(num / numLookup[i]);
+        num = num -(quotient * numLookup[i]);
+        for(var j=0;j<quotient;j++) {
+            result+= romanLookUp[i]
+        }
+    }
+    res.send(result);   
 }
 )
 
